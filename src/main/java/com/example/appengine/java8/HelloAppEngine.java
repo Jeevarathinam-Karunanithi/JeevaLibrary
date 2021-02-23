66ad13de088e3be1c0207b9f1d569296dc9c920c
arjunkumar-udainath <arjunkumar@anywhere.co>
2021-02-23 16:23:14 +0530

    spring with sample controllers

--- src/main/java/com/example/appengine/java8/HelloAppEngine.java
+++ src/main/java/com/example/appengine/java8/HelloAppEngine.java
@@ -23,10 +23,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 // With @WebServlet annotation the webapp/WEB-INF/web.xml is no longer required.
-@WebServlet(name = "HelloAppEngine", value = "/hello")
+//@WebServlet(name = "HelloAppEngine", value = "/hello")
+@Controller
 public class HelloAppEngine extends HttpServlet {
+
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String sample(
+      HttpServletRequest request, HttpServletResponse response) {
+          System.out.println("hello");
+    return "index";
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
---
+++ src/main/java/com/example/appengine/java8/SampleController.java
@@ -0,0 +1,20 @@
+package com.example.appengine.java8;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class SampleController {
+    
+
+    @RequestMapping(value = "/sample", method = RequestMethod.GET)
+  public @ResponseBody String sample(
+      HttpServletRequest request, HttpServletResponse response) {
+          System.out.println("hello");
+    return "hello";
+  }
+}
