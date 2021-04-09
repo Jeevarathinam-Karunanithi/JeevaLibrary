3b5a97103ea2eb4387fe2287796eb5b4fad0bb75
jeevarathinam-Karunanithi <jeeva.karun@anywhere.co>
2021-04-09 16:00:46 +0530

    Added a new component

---
+++ src/app/helloworld.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'sample',
+    template: `<h2>HELLO WORLD</h2>
+    <h2> This is demo Anguler Project </h2>`
+})
+export class HelloworldComponent {
+
+}
